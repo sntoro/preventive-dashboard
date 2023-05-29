75892c8a628ae6759ae0d32c864e69147f73d40a
sntoro <sofiyantoro1@gmail.com>
2023-05-29 09:51:27 +0700

    initiate

---
+++ app.js
@@ -0,0 +1,172 @@
+require("dotenv").config();
+var http = require("http");
+var express = require("express");
+const sql = require("mssql");
+const app = express();
+var server = http.createServer(app);
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+  },
+});
+const EventEmitter = require("events");
+const eventEmitter = new EventEmitter();
+eventEmitter.setMaxListeners(10);
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+const port = process.env.PORT;
+const interval = process.env.INTERVAL;
+
+app.get("/", function (req, res) {
+  res.sendStatus(200);
+});
+
+app.get("/dies", function (req, res) {
+  res.render("mte-dies", {
+    data: {
+      part: "DIES",
+    },
+  });
+});
+
+app.get("/doorframe", function (req, res) {
+  res.render("mte-doorframe", {
+    data: {
+      part: "DOORFRAME",
+    },
+  });
+});
+
+app.get("/machine", function (req, res) {
+  res.render("mte-machine", {
+    data: {
+      part: "MACHINE",
+    },
+  });
+});
+
+app.get("/jig", function (req, res) {
+  res.render("mte-jig", {
+    data: {
+      part: "JIG",
+    },
+  });
+});
+
+app.get("/mold", function (req, res) {
+  // eslint-disable-next-line no-undef
+  res.sendFile("mte-mold.html", { root: __dirname });
+});
+
+app.get("/diesmold", function (req, res) {
+  // eslint-disable-next-line no-undef
+  res.sendFile("mte-dies_mold.html", { root: __dirname });
+});
+
+app.get("/electrode", function (req, res) {
+  // eslint-disable-next-line no-undef
+  res.sendFile("mte-electrode.html", { root: __dirname });
+});
+
+const sqlConfig = {
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  server: process.env.DB_HOST,
+  pool: {
+    max: 10,
+    min: 0,
+    idleTimeoutMillis: 30000,
+  },
+  options: {
+    encrypt: false, // for azure
+    trustServerCertificate: true, // change to true for local dev / self-signed certs
+  },
+};
+
+setInterval(function () {
+  sql.connect(sqlConfig, (err) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+
+    new sql.Request().execute(
+      "MTE.zsp_get_preventive_mte_mold",
+      (err, result) => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        io.emit("data_prev_mold", result.recordset);
+      }
+    );
+
+    new sql.Request().execute(
+      "MTE.zsp_get_preventive_mte_dies",
+      (err, result) => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        io.emit("data_prev_dies", result.recordset);
+      }
+    );
+
+    new sql.Request().execute(
+      "MTE.zsp_get_preventive_mte_df",
+      (err, result) => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        io.emit("data_prev_df", result.recordset);
+      }
+    );
+
+    new sql.Request().execute(
+      "MTE.zsp_get_preventive_mte_machine",
+      (err, result) => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        io.emit("data_prev_machine", result.recordset);
+      }
+    );
+
+    new sql.Request().execute(
+      "MTE.zsp_get_preventive_mte_jig",
+      (err, result) => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        io.emit("data_prev_jig", result.recordset);
+      }
+    );
+
+    new sql.Request().execute(
+      "MTE.zsp_get_preventive_mte_electrode",
+      (err, result) => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        io.emit("data_prev_electrode", result.recordset);
+      }
+    );
+  });
+
+  sql.on("error", (err) => {
+    console.error(err);
+  });
+}, interval);
+
+io.on("connection", function (socket) {});
+
+server.listen(port, function () {
+  console.log("running on:" + port);
+});
